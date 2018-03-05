86d55f580d7c7a65de0382bc22d2d2bd9c17eb1f
nicolasmanic <nicolas.kanakis@gmail.com>
2018-03-05 20:52:09 +0200

    Code clean up ( package rename + gofmt)

--- model/note.go
+++ model/note.go
@@ -1,53 +1,55 @@
-package models
+package model
 
 import "time"
 
 //Note is the note we want to keep.
-type Note struct{
-	ID	int64  
-	Title string 
-	Memo string 
-	Created time.Time 
-	LastUpdated time.Time 
-	Tags map[string]bool 
-	Notebook *Notebook 
+type Note struct {
+	ID          int64
+	Title       string
+	Memo        string
+	Created     time.Time
+	LastUpdated time.Time
+	Tags        map[string]bool
+	Notebook    *Notebook
 }
 
 //NewNote returns a new note pointer.
-func NewNote(title, memo string, notebook *Notebook, tags []string) *Note{
+func NewNote(title, memo string, notebook *Notebook, tags []string) *Note {
 	note := &Note{
-		Title: title,
-		Memo: memo,
-		Created: time.Now(),
+		Title:       title,
+		Memo:        memo,
+		Created:     time.Now(),
 		LastUpdated: time.Now(),
-		Tags: make(map[string]bool),
-		Notebook: notebook,
+		Tags:        make(map[string]bool),
+		Notebook:    notebook,
 	}
 	note.AddTags(tags)
 	return note
 }
+
 //UpdateTitle updates the title of the note (also updates the LastUpdate value)
-func (note *Note) UpdateTitle(newTitle string){
+func (note *Note) UpdateTitle(newTitle string) {
 	note.Title = newTitle
 	noteUpdated(note)
 }
+
 //UpdateMemo updates the memo (also updates the LastUpdate value)
-func (note *Note) UpdateMemo(newMemo string){
+func (note *Note) UpdateMemo(newMemo string) {
 	note.Memo = newMemo
 	noteUpdated(note)
 }
 
 //AddTags add one or more tags to note (also updates the LastUpdate value)
-func (note *Note) AddTags(tags []string){
-	for _,tag:= range tags{
+func (note *Note) AddTags(tags []string) {
+	for _, tag := range tags {
 		note.Tags[tag] = true
 	}
 	noteUpdated(note)
 }
 
 //RemoveTags removes tags (also updates the LastUpdate value)
-func (note *Note) RemoveTags(tags []string){
-	for _,tag:= range tags{
+func (note *Note) RemoveTags(tags []string) {
+	for _, tag := range tags {
 		delete(note.Tags, tag)
 	}
 	noteUpdated(note)
@@ -55,12 +57,12 @@ func (note *Note) RemoveTags(tags []string){
 
 //UpdateNotebook updates the notebook this note belongs to
 //(also updates the LastUpdate value)
-func (note *Note) UpdateNotebook(notebook *Notebook){
+func (note *Note) UpdateNotebook(notebook *Notebook) {
 	note.Notebook = notebook
 	noteUpdated(note)
 }
 
 //Update the LastUpdated value with the current time
-func noteUpdated(note *Note){
+func noteUpdated(note *Note) {
 	note.LastUpdated = time.Now()
 }
--- model/notebook.go
+++ model/notebook.go
@@ -1,22 +1,24 @@
-package models
+package model
 
 //Notebook holds a list of "similar" notes
-type Notebook struct{
-	Notes map[int64]*Note 
-	Title string 
+type Notebook struct {
+	ID    int64
+	Notes map[int64]*Note
+	Title string
 }
 
 //NewNotebook returns a Notebook pointer
-func NewNotebook(title string) *Notebook{
+func NewNotebook(title string) *Notebook {
 	return &Notebook{
+		ID:    0,
 		Notes: make(map[int64]*Note),
 		Title: title,
 	}
 }
 
 //AddNote adds note to Notebook list, also update the note with current Notebook
-func (nBook *Notebook) AddNote(note *Note) bool{
-	if doesNoteExist(nBook, note.ID){
+func (nBook *Notebook) AddNote(note *Note) bool {
+	if doesNoteExist(nBook, note.ID) {
 		return false
 	}
 	nBook.Notes[note.ID] = note
@@ -26,15 +28,15 @@ func (nBook *Notebook) AddNote(note *Note) bool{
 
 //RemoveNote remove note from Notebook list.
 //WARNING: when removing a note we should add it to a default notebook.
-func (nBook *Notebook) RemoveNote(noteID int64) bool{
-	if !doesNoteExist(nBook, noteID){
+func (nBook *Notebook) RemoveNote(noteID int64) bool {
+	if !doesNoteExist(nBook, noteID) {
 		return false
 	}
 	delete(nBook.Notes, noteID)
 	return true
 }
 
-func doesNoteExist(notebook *Notebook, noteID int64) bool{
-	_, exists:= notebook.Notes[noteID]
+func doesNoteExist(notebook *Notebook, noteID int64) bool {
+	_, exists := notebook.Notes[noteID]
 	return exists
 }
