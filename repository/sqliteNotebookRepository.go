4e2447d8a6ed50c67ea25a81edea732b030a9e9f
nicolasmanic <nicolas.kanakis@gmail.com>
2018-03-11 00:02:47 +0200

    Add CRUD operations for Notebook

---
+++ repository/sqliteNotebookRepository.go
@@ -0,0 +1,182 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/nicolasmanic/tefter/model"
+)
+
+type sqliteNotebookRepository struct {
+	dbPath string
+	*sqlx.DB
+}
+
+//NewNotebookRepository returns a NotebookRepository interface
+func NewNotebookRepository(dbPath string) NotebookRepository {
+	db := connect2DB(dbPath)
+	return &sqliteNotebookRepository{dbPath, db}
+}
+
+func (notebookRepo *sqliteNotebookRepository) SaveNotebook(notebook *model.Notebook) (notebookID int64, err error) {
+	if notebook.Title == "" {
+		return -1, fmt.Errorf("Notebook should contain title")
+	}
+
+	tx, err := notebookRepo.Beginx()
+	if err != nil {
+		return -1, err
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			panicErr, _ := r.(error)
+			tx.Rollback()
+			notebookID = -1
+			err = panicErr
+		}
+	}()
+
+	result := tx.MustExec(`INSERT INTO notebook (title)	VALUES(?)`,
+		notebook.Title)
+	notebookID, err = result.LastInsertId()
+	checkError(err)
+	notebook.ID = notebookID
+	
+	err = tx.Commit()
+	checkError(err)
+	return notebookID, err
+}
+
+func (notebookRepo *sqliteNotebookRepository) GetNotebooks(notebooksIDs []int64) (notebooks []*model.Notebook, err error) {
+	notebooksIDs = removeDups(notebooksIDs)
+	if len(notebooksIDs) == 0 {
+		return []*model.Notebook{}, nil
+	}
+	
+	selectNotebook := "SELECT id, title FROM notebook "
+	whereIdIn := "WHERE id IN ("
+	args := []interface{}{}
+
+	for _, id := range notebooksIDs {
+		whereIdIn = whereIdIn + "?,"
+		args = append(args, id)
+	}
+	whereIdIn = whereIdIn[:len(whereIdIn)-1]
+	whereIdIn = whereIdIn + ")"
+	querynotebook := selectNotebook + whereIdIn
+
+	err = notebookRepo.Select(&notebooks, querynotebook, args...)
+	checkError(err)
+	
+	noteRepo := NewNoteRepository(notebookRepo.dbPath)
+	defer noteRepo.CloseDB()
+
+	for _, notebook := range notebooks{
+		noteIDs := notebookRepo.getNoteIDs(notebook.ID)
+		notes, err:= noteRepo.GetNotes(noteIDs)
+		checkError(err)
+		notebook.Notes = make(map[int64]*model.Note)
+		for _, note := range notes {
+			notebook.Notes[note.ID] = note
+		}
+	}
+
+	return notebooks, err
+}
+
+func (notebookRepo *sqliteNotebookRepository) GetNotebook(notebookID int64) (*model.Notebook, error) {
+	notebooks, err := notebookRepo.GetNotebooks([]int64{notebookID})
+	checkError(err)
+
+	if len(notebooks) != 1 {
+		return nil, fmt.Errorf("Could find notebook with id: %v", notebookID)
+	}
+	return notebooks[0], err
+}
+
+func (notebookRepo *sqliteNotebookRepository) UpdateNotebook(notebook *model.Notebook) (err error) {
+	if notebook.Title == "" {
+		return  fmt.Errorf("Notebook should contain title")
+	}
+
+	tx, err := notebookRepo.Beginx()
+	if err != nil {
+		return err
+	}
+	
+	defer func() {
+		if r := recover(); r != nil {
+			panicErr, _ := r.(error)
+			tx.Rollback()
+			err = panicErr
+		}
+	}()
+		
+	updateNotebookQuery := `UPDATE notebook SET	title = ? WHERE id = ?`
+	tx.MustExec(updateNotebookQuery, notebook.Title, notebook.ID)
+
+	err = tx.Commit()
+	checkError(err)
+	return err
+}
+
+func (notebookRepo *sqliteNotebookRepository) DeleteNotebooks(notebooksIDs []int64) error {
+	notebooksIDs = removeDups(notebooksIDs)
+	if len(notebooksIDs) == 0 {
+		return nil
+	}
+	
+	whereIdIn := "WHERE id IN ("
+	args := []interface{}{}
+
+	for _, id := range notebooksIDs {
+		whereIdIn = whereIdIn + "?,"
+		args = append(args, id)
+	}
+	whereIdIn = whereIdIn[:len(whereIdIn)-1]
+	whereIdIn = whereIdIn + ")"
+	deleteNotebook := "DELETE FROM notebook " + whereIdIn
+
+	tx, err := notebookRepo.Beginx()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			panicErr, _ := r.(error)
+			tx.Rollback()
+			err = panicErr
+		}
+	}()
+
+	tx.MustExec(deleteNotebook, args...)
+	err = tx.Commit()
+	checkError(err)
+
+	noteRepo := NewNoteRepository(notebookRepo.dbPath)
+	defer noteRepo.CloseDB()
+	for _, notebookID := range notebooksIDs{
+		noteIDs := notebookRepo.getNoteIDs(notebookID)
+		err:= noteRepo.DeleteNotes(noteIDs)
+		checkError(err)	
+	}
+
+	return  err
+}
+
+func (notebookRepo *sqliteNotebookRepository) DeleteNotebook(notebookID int64) error {
+	return notebookRepo.DeleteNotebooks([]int64{notebookID})
+}
+
+func (notebookRepo *sqliteNotebookRepository) CloseDB() error {
+	return notebookRepo.Close()
+}
+
+func (notebookRepo *sqliteNotebookRepository) getNoteIDs(notebookID int64) []int64{
+	query:="SELECT note_id FROM notebook_note WHERE notebook_id = ?"
+	noteIDs:=[]int64{}
+	err := notebookRepo.Select(&noteIDs, query, []interface{}{notebookID}...)
+	checkError(err)
+	return noteIDs
+}
---
+++ repository/sqliteNotebookRepository_test.go
@@ -0,0 +1,129 @@
+package repository
+
+import (
+	"testing"
+	"os"
+	"github.com/nicolasmanic/tefter/model"
+)
+
+func TestSaveNotebook(t *testing.T){
+	testRepo := NewNotebookRepository("test.db")
+	//tear down test
+	defer func() {
+		testRepo.CloseDB()
+		os.Remove("test.db")
+	}()
+
+	mockNotebook := model.NewNotebook("testTitle")
+	id, err:= testRepo.SaveNotebook(mockNotebook)
+	
+	if err != nil {
+		t.Errorf("Could not save notebook to DB, error msg: %v", err)
+	}
+
+	if id != mockNotebook.ID {
+		t.Error("Could not save correctly notebook to DB")
+	}
+}
+
+func TestGetNotebooks(t *testing.T){
+	testRepo := NewNotebookRepository("test.db")
+	testNoteRepo := NewNoteRepository("test.db")
+	//tear down test
+	defer func() {
+		testRepo.CloseDB()
+		testNoteRepo.CloseDB()
+		os.Remove("test.db")
+	}()
+
+	mockNotebook2 := model.NewNotebook("notebook 2")
+	mockNotebook3 := model.NewNotebook("notebook 3")
+	testRepo.SaveNotebook(mockNotebook2)
+	testRepo.SaveNotebook(mockNotebook3)
+
+	mockNote1 := model.NewNote("testTitle", "test Memo", 2, []string{"testTag1", "testTag2"})
+	mockNote2 := model.NewNote("testTitle", "test Memo", 3, []string{"testTag3", "testTag4"})
+
+	testNoteRepo.SaveNote(mockNote1)
+	testNoteRepo.SaveNote(mockNote2)
+
+	notebooks, err:= testRepo.GetNotebooks([]int64{2,3})
+
+	if err != nil {
+		t.Errorf("Could not retrieve notebooks from DB, error msg: %v", err)
+	}
+
+	if len(notebooks) != 2{
+		t.Error("Could not retrieve notebooks from DB")
+	}
+	
+	for _, notebook := range notebooks{
+		if len(notebook.Notes) != 1{
+			t.Error("Could not retrieve notebooks from DB")
+		}
+	}
+	
+}
+
+func TestUpdateNotebook(t *testing.T){
+	testRepo := NewNotebookRepository("test.db")
+	//tear down test
+	defer func() {
+		testRepo.CloseDB()
+		os.Remove("test.db")
+	}()
+
+	mockNotebook := model.NewNotebook("test Title")
+	id, _:= testRepo.SaveNotebook(mockNotebook)
+	
+	mockNotebook.Title = "Updated Title"
+	
+	err := testRepo.UpdateNotebook(mockNotebook)
+	if err != nil {
+		t.Errorf("Could not update notebook, error msg: %v", err)
+	}
+	notebook, _ := testRepo.GetNotebook(id)
+
+	if notebook.Title != "Updated Title" {
+		t.Error("Could not update notebook")
+	}
+}
+
+func TestDeleteNotebooks(t *testing.T){
+	testRepo := NewNotebookRepository("test.db")
+	testNoteRepo := NewNoteRepository("test.db")
+	//tear down test
+	defer func() {
+		testRepo.CloseDB()
+		testNoteRepo.CloseDB()
+		os.Remove("test.db")
+	}()
+
+	mockNotebook2 := model.NewNotebook("notebook 2")
+	mockNotebook3 := model.NewNotebook("notebook 3")
+	testRepo.SaveNotebook(mockNotebook2)
+	testRepo.SaveNotebook(mockNotebook3)
+
+	mockNote1 := model.NewNote("testTitle", "test Memo", 2, []string{"testTag1", "testTag2"})
+	mockNote2 := model.NewNote("testTitle", "test Memo", 3, []string{"testTag3", "testTag4"})
+
+	testNoteRepo.SaveNote(mockNote1)
+	testNoteRepo.SaveNote(mockNote2)
+	
+	err := testRepo.DeleteNotebooks([]int64{2,3})
+	if err != nil {
+		t.Errorf("Could not delete notebooks from DB, error msg: %v", err)
+	}
+
+	notes, _:= testNoteRepo.GetNotes([]int64{mockNote1.ID,mockNote2.ID})
+	if len(notes) != 0{
+		t.Errorf("Could not delete notes of deleted notebook from DB")
+	}
+
+	notebooks, _:= testRepo.GetNotebooks([]int64{2,3})
+	if len(notebooks) != 0{
+		t.Errorf("Could not delete notebook from DB")
+	}
+}
+
+
