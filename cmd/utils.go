efac5de3166817fc2117bf57215c71f3db5bccaa
nicolasmanic <nicolas.kanakis@gmail.com>
2018-03-16 16:50:14 +0200

    Add accumulateNotes to gather notes from db & map2slice to transform map of Notes to slice of Notes

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/nicolasmanic/tefter/model"
 	"io"
 	"io/ioutil"
 	"os"
@@ -8,6 +10,7 @@ import (
 	"strings"
 )
 
+//TODO: unit test
 func openEditor(text string) (string, error) {
 	vi := "vim"
 	fpath := os.TempDir() + "/tmpMemo.txt"
@@ -52,4 +55,63 @@ func int2int64(input []int, initialLength int) []int64 {
 		result = append(result, int64(tmp))
 	}
 	return result
+}
+
+func accumulateNotes(ids []int, notebookTitles, tags []string, printAll bool) map[int64]*model.Note {
+	var notesMap = make(map[int64]*model.Note, 0)
+	if printAll {
+		allNotes, err := NoteDB.GetNotes([]int64{})
+		if err != nil {
+			fmt.Print("Error while retrieving notes by id")
+		} else {
+			for _, note := range allNotes {
+				notesMap[note.ID] = note
+			}
+		}
+		return notesMap
+	}
+	//Add notes based on id
+	if len(ids) > 0 {
+		idNotes, err := NoteDB.GetNotes(int2int64(ids, 0))
+		if err != nil {
+			fmt.Print("Error while retrieving notes by id")
+		} else {
+			for _, note := range idNotes {
+				notesMap[note.ID] = note
+			}
+		}
+	}
+	if len(notebookTitles) > 0 {
+		//Add notes based on notebook
+		for _, notebookTitle := range notebookTitles {
+			notebook, err := NotebookDB.GetNotebookByTitle(notebookTitle)
+			if err != nil {
+				fmt.Printf("Error while retrieving notebook for title: %v", notebookTitle)
+			} else if notebook != nil {
+				for _, note := range notebook.Notes {
+					notesMap[note.ID] = note
+				}
+
+			}
+		}
+	}
+	if len(tags) > 0 {
+		tagNotes, err := NoteDB.GetNotesByTag(tags)
+		if err != nil {
+			fmt.Print("Error while retrieving notes by tag")
+		} else {
+			for _, note := range tagNotes {
+				notesMap[note.ID] = note
+			}
+		}
+	}
+	return notesMap
+}
+
+func map2slice(m map[int64]*model.Note) []*model.Note {
+	notes := []*model.Note{}
+	for _, note := range m {
+		notes = append(notes, note)
+	}
+	return notes
 }
