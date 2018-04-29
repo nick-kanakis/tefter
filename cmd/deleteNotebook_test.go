09cea14be768c59a5e8615e17a2135d004151971
nicolasmanic <nicolas.kanakis@gmail.com>
2018-04-29 12:40:05 +0300

    Test cases for deleteNotebook & delete cmd

--- cmd/deleteNotebook_test.go
+++ cmd/deleteNotebook_test.go
@@ -1,43 +1,66 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/nicolasmanic/tefter/model"
 	"github.com/nicolasmanic/tefter/repository"
+	"reflect"
 	"testing"
 )
 
-func TestDeleteNotebooksNoArguments(t *testing.T) {
-	oldNotebookDB := NotebookDB
-	NotebookDB = mockNotebookDBDelete{}
-	defer func() {
-		NotebookDB = oldNotebookDB
-		if r := recover(); r == nil {
-			t.Errorf("Empty argument should cause the delete cmd to panic")
-		}
-	}()
-	deleteNotebooksWrapper(nil, []string{})
-}
-
 func TestDeleteNotebooks(t *testing.T) {
-	oldNotebookDB := NotebookDB
-	NotebookDB = mockNotebookDBDelete{}
-	//Restore interface
-	defer func() {
-		NotebookDB = oldNotebookDB
-	}()
-	deleteNotebooks([]string{"12"})
+	cases := []struct {
+		mDB         mockNotebookDBDelete
+		expectedErr error
+		titles      []string
+	}{
+		{
+			mDB: mockNotebookDBDelete{
+				notebook: model.NewNotebook("title"),
+				err:      nil,
+			},
+			expectedErr: nil,
+			titles:      []string{"title1", "title2"},
+		}, {
+			mDB:         mockNotebookDBDelete{},
+			expectedErr: errors.New("No argument passed, at least one notebook title should be provided"),
+			titles:      []string{},
+		}, {
+			mDB: mockNotebookDBDelete{
+				notebook: nil,
+				err:      errors.New("Unexpected error"),
+			},
+			expectedErr: errors.New("Could not retrieve notebook for title: title1 error msg: Unexpected error"),
+			titles:      []string{"title1"},
+		},
+	}
+
+	for _, c := range cases {
+		oldNotebookDB := NotebookDB
+		NotebookDB = c.mDB
+		defer func() {
+			NotebookDB = oldNotebookDB
+		}()
+
+		err := deleteNotebooks(c.titles)
+		if !reflect.DeepEqual(err, c.expectedErr) {
+			t.Errorf("Expected err to be %q but it was %q", c.expectedErr, err)
+		}
+	}
 }
 
 type mockNotebookDBDelete struct {
 	repository.NotebookRepository
+	notebook *model.Notebook
+	err      error
 }
 
 func (mDB mockNotebookDBDelete) DeleteNotebook(notebooksID int64) error {
-	return nil
+	return mDB.err
 }
 
 func (mDB mockNotebookDBDelete) GetNotebookByTitle(notebookTitle string) (*model.Notebook, error) {
-	notebook := model.NewNotebook(notebookTitle)
-	notebook.ID = 1
-	return notebook, nil
+	/*notebook := model.NewNotebook(notebookTitle)
+	notebook.ID = 1*/
+	return mDB.notebook, mDB.err
 }
--- cmd/delete_test.go
+++ cmd/delete_test.go
@@ -1,27 +1,63 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/nicolasmanic/tefter/repository"
+	"reflect"
 	"testing"
 )
 
-func TestDeleteWrapper(t *testing.T) {
-	oldNoteDB := NoteDB
-	NoteDB = mockNoteDBDelete{}
-	//Restore interface
-	defer func() {
-		NoteDB = oldNoteDB
-	}()
+func TestDeleteArgs(t *testing.T) {
+	cases := []struct {
+		args        []string
+		expectedErr error
+		mDB         mockNoteDBDelete
+	}{
+		{
+			args:        []string{"1", "2"},
+			expectedErr: nil,
+			mDB: mockNoteDBDelete{
+				err: nil,
+			},
+		}, {
+			args:        []string{},
+			expectedErr: nil,
+			mDB: mockNoteDBDelete{
+				err: nil,
+			},
+		}, {
+			args:        []string{"a"},
+			expectedErr: errors.New("Could note transform input to id for argument: a"),
+			mDB:         mockNoteDBDelete{},
+		}, {
+			args:        []string{"1", "2"},
+			expectedErr: errors.New("Error while deleting notes, error msg: Unexpected error"),
+			mDB: mockNoteDBDelete{
+				err: errors.New("Unexpected error"),
+			},
+		},
+	}
 
-	deleteWrapper(nil, []string{"1", "2"})
-	//empty id slice should not cause any problem
-	deleteWrapper(nil, []string{})
+	for _, c := range cases {
+		oldNoteDB := NoteDB
+		NoteDB = c.mDB
+		//Restore interface
+		defer func() {
+			NoteDB = oldNoteDB
+		}()
+
+		err := deleteArgs(c.args)
+		if !reflect.DeepEqual(c.expectedErr, err) {
+			t.Errorf("Expected err to be %q but it was %q", c.expectedErr, err)
+		}
+	}
 }
 
 type mockNoteDBDelete struct {
 	repository.NoteRepository
+	err error
 }
 
 func (mDB mockNoteDBDelete) DeleteNotes(noteIDs []int64) error {
-	return nil
+	return mDB.err
 }
